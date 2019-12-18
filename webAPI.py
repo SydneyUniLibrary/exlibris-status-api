fb3b1a9145a7f617b9ebe2f295474aa11b625bbc
Jim Nicholls <jim.nicholls@sydney.edu.au>
2019-12-18 14:26:08 +1100

    Lift dynamodb and table into the execution context.

--- webAPI.py
+++ webAPI.py
@@ -6,19 +6,22 @@ import boto3
 import os
 
 
+dynamodb = boto3.resource(
+    "dynamodb",
+    region_name=os.environ["region_name"],
+    aws_access_key_id=os.environ["access_key"],
+    aws_secret_access_key=os.environ["secret_access"],
+)
+
+table = dynamodb.Table(os.environ["table"])
+
+
 def handler(event, context):
     headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": True,
     }
     try:
-        dynamodb = boto3.resource(
-            "dynamodb",
-            region_name=os.environ["region_name"],
-            aws_access_key_id=os.environ["access_key"],
-            aws_secret_access_key=os.environ["secret_access"],
-        )
-        table = dynamodb.Table(os.environ["table"])
         response = table.get_item(Key={"product": "Primo"})
         return {
             "statusCode": 200,
